6877a627bd0a63516ab9f4f4bbe811c28a3c9830
henry-95 <henry@northens.co.uk>
2022-11-25 21:13:32 +0000

    Updates

--- js/templates/navigation.js
+++ js/templates/navigation.js
@@ -1,5 +1,6 @@
 const navigation = document.createElement("nav");
-navigation.innerHTML = '<ul><li><a href="index.html">Home</a></li><li><a href="projects.html">Projects</a></li></ul>';
+const title = document.title;
+navigation.innerHTML = '<ul><li><a href="index.html" class="' + (title == "index" ? "active" : "" ) + '">Home</a></li><li><a href="projects.html" class="' + (title == "projects" ? "active" : "" ) + '">Projects</a></li></ul>';
 
 export default navigation;
 
