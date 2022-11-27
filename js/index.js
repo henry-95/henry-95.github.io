01a370e30319cf9379b0550572e798f662e079f0
henry-95 <henry@northens.co.uk>
2022-11-27 10:11:59 +0000

    Updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import navigation from '/js/templates/navigation.js';
 import footer from '/js/templates/footer.js';
 
-document.getElementById("main").prepend(navigation);
+document.getElementById("header").append(navigation);
 document.getElementById("main").append(footer);
--- js/templates/navigation.js
+++ js/templates/navigation.js
@@ -1,6 +1,6 @@
 const navigation = document.createElement("nav");
 const title = document.title;
-navigation.innerHTML = '<ul><li><a href="index.html" class="' + (title == "index" ? "active" : "" ) + '">Home</a></li><li><a href="projects.html" class="' + (title == "projects" ? "active" : "" ) + '">Projects</a></li></ul>';
+navigation.innerHTML = '<ul><li><a href="#home"><i class="fa-sharp fa-solid fa-house"></i></a></li><li><a href="#projects">Projects</a></li><li><a href="#about">About Me</a></li><li><a href="#contact">Contact Me</a></li></ul>';
 
 export default navigation;
 
