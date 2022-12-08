60fbcadcaef2cd28a27dec06a66f42b33900d8aa
henry-95 <henry@northens.co.uk>
2022-12-08 22:34:28 +0000

    Update

--- js/index2.js
+++ js/index2.js
@@ -5,8 +5,17 @@ for(let section of sections){
     section.addEventListener("click", clickSection, false);
 }
 
+document.getElementById("toggle-lights").addEventListener("click", toggleLights, false);
+
+function toggleLights(){
+    this.children[0].classList.toggle("lights-off");
+    this.children[0].classList.toggle("fa-sun");
+    this.children[0].classList.toggle("fa-moon");
+}
+
 function clickSection(){
     let sectionId = this.id;
+    console.log(this);
     
     for(let section of sections){
         if(sectionId != section.id)
